71eec018211b2b2dbc818ea09e3b42c7fe19310e
seraluda <78064782+seraluda@users.noreply.github.com>
2021-02-14 02:31:06 +0700

    Update seraluda-download.js

--- seraluda-download.js
+++ seraluda-download.js
@@ -18,7 +18,13 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://worldtrendlifestyle.blogspot.com/p/download.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*4)+ 1);
+var xxx=null;
+if(x=="1"){xxx="american-football.html"}
+if(x=="2"){xxx="kim-kardashian.html"}
+if(x=="3"){xxx="taylor-swift.html"}
+if(x=="4"){xxx="among-us.html"}a_to_ve[i].href="https://worldtrendlifestyle.blogspot.com/2021/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
