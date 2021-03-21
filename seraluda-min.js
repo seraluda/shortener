a96be902ada79057661a689da46fb90b83a6952d
seraluda.com <78064782+seraluda@users.noreply.github.com>
2021-03-22 01:50:37 +0700

    Update seraluda-min.js

--- seraluda-min.js
+++ seraluda-min.js
@@ -119,7 +119,7 @@ if(x=="95"){xxx="british-american-tobacco.html"}
 if(x=="96"){xxx="broadcom.html"}
 if(x=="97"){xxx="hitachi.html"}
 if(x=="98"){xxx="oppo.html"}      
-if(x=="99"){xxx="vivo.html"}a_to_ve[i].href="https://seraluda.com/2021/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="99"){xxx="vivo.html"}a_to_ve[i].href="https://www.seraluda.com/2021/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
